3659a571888db1477673c2ef3da95ee1e52ad5b6
Daniel <845765@qq.com>
2026-02-22 15:58:59 +0800

    :art: Improve keyboard pop on HarmonyOS (Pura X)
    Signed-off-by: Daniel <845765@qq.com>

--- app/src/mobile/util/mobileAppUtil.ts
+++ app/src/mobile/util/mobileAppUtil.ts
@@ -1,6 +1,12 @@
 import {hasClosestByAttribute, hasClosestByClassName} from "../../protyle/util/hasClosest";
 
+export let keyboardLockUntil = 0;
+
 export const callMobileAppShowKeyboard = () => {
+    // 某些机型（比如鸿蒙 Pura X）在弹起键盘后会立即触发 activeBlur 导致键盘被关闭，所以在主动唤起键盘时锁定一段时间，禁止 activeBlur 关闭键盘
+    // 每次主动唤起键盘时，锁定接下来的 300ms 不允许通过 activeBlur 关闭
+    keyboardLockUntil = Date.now() + 200;
+
     if (window.JSAndroid && window.JSAndroid.showKeyboard) {
         window.JSAndroid.showKeyboard();
     } else if (window.JSHarmony && window.JSHarmony.showKeyboard) {
